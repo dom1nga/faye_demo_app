cbb83a897dbefb5056577ba796684b14e41054f5
dom1nga <beloved.dom1nga@gmail.com>
2011-11-06 03:26:31 +0200

    Controller methods moved to gem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,4 @@ class ApplicationController < ActionController::Base
   before_filter :find_faye_client
   before_filter :set_faye_client_for_observer
   protect_from_forgery
-
-  private
-
-  def find_faye_client
-    @faye_client = env['faye.client']
-  end
-
-  def set_faye_client_for_observer
-    ActionObserver.faye_client = env['faye.client']
-  end
 end
