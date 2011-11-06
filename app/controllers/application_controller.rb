6b4e6b8cdb205a8ea1b5427f8dd5f00151b14fd5
dom1nga <beloved.dom1nga@gmail.com>
2011-11-06 10:44:33 +0200

    Refactoring.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_filter :find_faye_client
-  before_filter :set_faye_client_for_observer
+  before_filter :set_faye_client
   protect_from_forgery
 end
